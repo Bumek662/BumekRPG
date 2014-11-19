f2552e1d001e5529c2c2a1e5f4bb31741d0c6703
Bumek662 <Bumek662@gmail.com>
2014-11-19 16:40:07 +0100

    S

---
+++ BumekRPG/BumekRPG/Class/Monsters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BumekRPG.Class
+{
+    class Monsters
+    {
+        public static string ListOfMonsters = "1) Królik (1 lvl)\n2) Pies (2 lvl)\n3) Wilk (4 lvl)";
+        public static string Monster_Name { get; set; }
+        public static int Monster_AD { get; set; }
+        public static int Monster_HP { get; set; }
+        public static int Monster_Lvl { get; set; }
+        public static int Monster_Exp { get; set; }
+        public static int Monster_MaxLvlDifference { get; set; }
+        public static int Monster_MaxDrop { get; set; }
+
+        public static void Monster(int MonsterID)
+        {
+
+            if (MonsterID == 1)
+            {
+                Monster_Name = "Krolik";
+                Monster_Lvl = 1;
+                Monster_AD = 4;
+                Monster_HP = 10;
+                Monster_MaxLvlDifference = 0;
+                Monster_MaxDrop = 15;
+                Monster_Exp = 25;
+            }
+
+            if (MonsterID == 2)
+            {
+                Monster_Name = "Pies";
+                Monster_Lvl = 2;
+                Monster_AD = 7;
+                Monster_HP = 15;
+                Monster_MaxLvlDifference = 1;
+                Monster_MaxDrop = 30;
+                Monster_Exp = 40;
+            }
+
+            if (MonsterID == 3)
+            {
+                Monster_Name = "Wilk";
+                Monster_Lvl = 4;
+                Monster_AD = 10;
+                Monster_HP = 24;
+                Monster_MaxLvlDifference = 2;
+                Monster_MaxDrop = 45;
+                Monster_Exp = 75;
+            }
+
+        }
+    }
+}
---
+++ BumekRPG/BumekRPG/Class/Shop.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BumekRPG.Class
+{
+    public class Shop
+    {
+        public static void OpenShop()
+        {
+            string ListOfItems = "1) Mały eliksir (+25% zdrowia) - 50$\n2) Średni eliksir (+60% zdrowia) - 80$\n3) Duży eliksir (+100% zdrowia) - 140$" + "\nTwoje pieniądze: " + Character.Money + "$\n:";
+            Console.WriteLine("--------------------------\n----------Sklep-----------\n--------------------------"); // Naglowek
+            Console.Write(ListOfItems); // Przedmioty
+            int ChosenItem = Convert.ToInt32(Console.ReadLine());
+            BuyItem(ChosenItem);
+
+        }
+
+
+        public static void BuyItem(int ItemID)
+        {
+            int Item_Cost;
+            if (ItemID == 1)
+            {
+                Item_Cost = 25;
+                if (Character.HP != Character.MaxHP)
+                {
+                    if (Character.Money >= Item_Cost)
+                    {
+                        Character.Money -= Item_Cost;
+                        if (Character.HP + Character.MaxHP / 4 <= Character.MaxHP)
+                        {
+                            int Counted = Character.MaxHP / 4;
+                            Character.HP += Counted;
+                            Console.WriteLine("Poprawnie zakupiłeś mały eliksir!\nZostałeś uleczony o 25% maksymalnego zdrowia (" + Counted + ").\nAktualny stan konta: " + Character.Money);
+                        }
+                        else
+                        {
+                            if (Character.HP != Character.MaxHP)
+                            {
+                                Console.WriteLine("Poprawnie zakupiłeś mały eliksir!\nZostałeś uleczony do 100% zdrowia nie wykorzystując całego eliksiru.\nAktualny stan konta: " + Character.Money);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Masz za mało pieniędzy.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Jesteś zdrowy!");
+                }
+            }
+
+            // ############# Koniec itemu o ID 1. #############
+
+            if (ItemID == 2)
+            {
+                Item_Cost = 80;
+                if (Character.HP != Character.MaxHP)
+                {
+                    if (Character.Money >= Item_Cost)
+                    {
+                        Character.Money -= Item_Cost;
+                        if (Character.HP + Character.MaxHP / 10 * 6 <= Character.MaxHP)
+                        {
+                            int Counted = Character.MaxHP / 10 * 6;
+                            Character.HP += Counted;
+                            Console.WriteLine("Poprawnie zakupiłeś mały eliksir!\nZostałeś uleczony o 60% maksymalnego zdrowia (" + Counted + ").\nAktualny stan konta: " + Character.Money);
+                        }
+                        else
+                        {
+                            if (Character.HP != Character.MaxHP)
+                            {
+                                Console.WriteLine("Poprawnie zakupiłeś średni eliksir!\nZostałeś uleczony do 100% zdrowia nie wykorzystując całego eliksiru.\nAktualny stan konta: " + Character.Money);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Masz za mało pieniędzy.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Jesteś zdrowy!");
+                }
+            } 
+
+            // ############# Koniec itemu o ID 2. #############
+
+            if (ItemID == 3)
+            {
+                Item_Cost = 140;
+                if (Character.HP != Character.MaxHP)
+                {
+                    if (Character.Money >= Item_Cost)
+                    {
+                        Character.Money -= Item_Cost;
+                        Character.HP = Character.MaxHP;
+                        Console.WriteLine("Poprawnie zakupiłeś duży eliksir!\nZostałeś uleczony do 100% zdrowia.\nAktualny stan konta: " + Character.Money);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Masz za mało pieniędzy.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Jesteś zdrowy!");
+                }
+            }
+
+            // ############# Koniec itemu o ID 3. #############
+
+        }
+
+    }
+}
